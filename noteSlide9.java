24063fb538fb51f3601d22e46a521096407e55a4
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-11-11 12:29:22 +0800

    Create noteSlide9.java

---
+++ noteSlide9.java
@@ -0,0 +1,19 @@
+package selfProgramming;
+
+public class noteSlide9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int amount = 20;
+		int balance = 100;
+	
+		
+		if (amount <= balance )
+			balance = balance-amount;
+		
+		System.out.println(balance);
+		
+		
+	}
+
+}
