718a4ecdb673e713023b9b4a0c87309b5f73a006
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-11-11 12:40:09 +0800

    Create note2Slide35.java

---
+++ note2Slide35.java
@@ -0,0 +1,27 @@
+package selfProgramming;
+
+public class note2Slide35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch = 'b';
+		switch (ch)
+		{
+			case 'a' :
+				System.out.println("ch=a");
+				break;
+			case 'b' :
+				System.out.println("ch=b");
+				break;
+			case 'c' :
+				System.out.println("ch=c");
+				break;
+			default :
+				System.out.println("Default");
+		
+		}
+		
+	}
+
+}
