fddb22ebbb4dde1e0ca3d88bbdd90c8138a76344
William Ng Wei Yi <92504689+William284613@users.noreply.github.com>
2021-11-11 12:31:04 +0800

    Create noteSlide14.java

---
+++ noteSlide14.java
@@ -0,0 +1,22 @@
+package selfProgramming;
+
+public class noteSlide14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int total = 50;
+		int max = 30;
+		int errorCount = 200;
+		
+		if (total>max)
+		{
+			System.out.println("Error!!");
+			errorCount++;
+			System.out.println(errorCount);
+		}
+		
+		
+	}
+
+}
